1107fba3105de28f4fc81b4488ea61673161f371
Josephfxw <Josephxwf@gmail.com>
2015-07-11 19:06:18 -0400

    add login about

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/home'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -18,6 +19,7 @@ Rails.application.routes.draw do
   resources :sessions
 
   get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
