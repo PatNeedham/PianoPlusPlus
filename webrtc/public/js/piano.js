72f7b0c417468763d028d7e60416fa90e28bc488
Sharon Lin <sharon.lin.14@gmail.com>
2015-07-12 10:11:43 -0400

    Additions

--- webrtc/public/js/piano.js
+++ webrtc/public/js/piano.js
@@ -1,139 +1,100 @@
-/**
-* NOTE:
-* Some of the features of the  Web Audio API currently only work on Chrome, hence the 'webkit' prefix!
+/*
+
+Is you use this in a project, drop me a line. I'd love to see it used in action!
+
 */
+$(document).ready(function(){
+  var c = $("#mid-c")[0];
+  var cs = $("#mid-c-sharp")[0];
+  var d = $("#mid-d")[0];
+  var ds = $("#mid-d-sharp")[0];
+  var e = $("#mid-e")[0];
+  var f = $("#mid-f")[0];
+  var fs = $("#mid-f-sharp")[0];
+  var g = $("#mid-g")[0];
+  var gs = $("#mid-g-sharp")[0];
+  var a = $("#mid-a")[0];
+  var as = $("#mid-a-sharp")[0];
+  var b = $("#mid-b")[0];
 
-(function() {
-    // Create audio (context) container
-    var audioCtx = new (AudioContext || webkitAudioContext)();
+  //c.play();
+  $("li").click( function(){
+    var note = this.dataset.note;
+    //alert(note);
+   switch(note){
+      case "a": a.play();
+        break;
+      case "b": b.play();
+        break;
+      case "c": c.play();
+        break;
+      case "d": d.play();
+        break;
+      
+      case "e": e.play();
+        break;
+      
+      case "f": f.play();
+        break;
+      
+      case "g": g.play();
+        break;
+      
+      case "as": as.play();
+        break;
+      
+      case "cs": cs.play();
+        break;
+      
+      case "ds": ds.play();
+        break;
+      
+      case "fs": fs.play();
+        break;
+      
+      case "gs": gs.play();
+        break;
+      }
+  });  
 
-    // Table of notes with correspending keyboard codes. Frequencies are in hertz.
-    // The notes start from middle C
-    var notesByKeyCode = {
-        65: { noteName: 'c4', frequency: 261.6, keyName: 'a' },
-        83: { noteName: 'd4', frequency: 293.7, keyName: 's' },
-        68: { noteName: 'e4', frequency: 329.6, keyName: 'd' },
-        70: { noteName: 'f4', frequency: 349.2, keyName: 'f' },
-        71: { noteName: 'g4', frequency: 392, keyName: 'g' },
-        72: { noteName: 'a4', frequency: 440, keyName: 'h' },
-        74: { noteName: 'b4', frequency: 493.9, keyName: 'j' },
-        75: { noteName: 'c5', frequency: 523.3, keyName: 'k' },
-        76: { noteName: 'd5', frequency: 587.3, keyName: 'l' },
-        186: { noteName: 'e5', frequency: 659.3, keyName: ';' }
-    };
+});
+//audio.play();
 
-    function Key(noteName, keyName, frequency) {
-        var keyHTML = document.createElement('div');
-        var keySound = new Sound(frequency, 'triangle');
+// Javascript adapted from here: 
+// http://www.html5rocks.com/en/tutorials/webaudio/intro/
 
-        /* Style the key */
-        keyHTML.className = 'key';
-        keyHTML.innerHTML = noteName + '<br><span>' + keyName + '</span>';
+// Notes can be found here:
+// http://www.vibrationdata.com/piano.htm
+/*
+window.onload = init;
+var context;
+var bufferLoader;
 
-        return {
-            html: keyHTML,
-            sound: keySound
-        };
-    }
+function init() {
+  context = new webkitAudioContext();
 
-    function Sound(frequency, type) {
-        this.osc = audioCtx.createOscillator(); // Create oscillator node
-        this.pressed = false; // flag to indicate if sound is playing
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      'http://www.vibrationdata.com/piano_middle_C.mp3',
+      'http://www.vibrationdata.com/piano_middle_C.mp3',
+    ],
+    finishedLoading
+    );
 
-        /* Set default configuration for sound */
-        if(typeof frequency !== 'undefined') {
-            /* Set frequency. If it's not set, the default is used (440Hz) */
-            this.osc.frequency.value = frequency;
-        }
+  bufferLoader.load();
+}
 
-        /* Set waveform type. Default is actually 'sine' but triangle sounds better :) */
-        this.osc.type = type || 'triangle';
+function finishedLoading(bufferList) {
+  // Create two sources and play them both together.
+  var source1 = context.createBufferSource();
+  var source2 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
 
-        /* Start playing the sound. You won't hear it yet as the oscillator node needs to be
-        piped to output (AKA your speakers). */
-        this.osc.start(0);
-    };
-
-    Sound.prototype.play = function() {
-        if(!this.pressed) {
-            this.pressed = true;
-            this.osc.connect(audioCtx.destination);
-        }
-    };
-
-    Sound.prototype.stop = function() {
-        this.pressed = false;
-        this.osc.disconnect();
-    };
-
-    function createKeyboard(notes, containerId) {
-        var sortedKeys = []; // Placeholder for keys to be sorted
-        var waveFormSelector = document.getElementById('soundType');
-
-        for(var keyCode in notes) {
-            var note = notes[keyCode];
-
-            /* Generate playable key */
-            note.key = new Key(note.noteName, note.keyName, note.frequency);
-
-            /* Add new key to array to be sorted */
-            sortedKeys.push(notes[keyCode]);
-        }
-
-        /* Sort keys by frequency so that they'll be added to the DOM in the correct order */
-        sortedKeys = sortedKeys.sort(function(note1, note2) {
-            if (note1.frequency < note2.frequency) return -1;
-            if (note1.frequency > note2.frequency) return 1;
-
-            return 0;
-        });
-
-        // Add those sorted keys to DOM
-        for(var i = 0; i < sortedKeys.length; i++) {
-            document.getElementById(containerId).appendChild(sortedKeys[i].key.html);
-        }
-
-        var playNote = function(event) {
-            var keyCode = event.keyCode;
-
-            if(typeof notesByKeyCode[keyCode] !== 'undefined') {
-                // Pipe sound to output (AKA speakers)
-                notesByKeyCode[keyCode].key.sound.play();
-
-                // Highlight key playing
-                notesByKeyCode[keyCode].key.html.className = 'key playing';
-            }
-        };
-
-        var endNote = function(event) {
-            var keyCode = event.keyCode;
-
-            if(typeof notesByKeyCode[keyCode] !== 'undefined') {
-                // Kill connection to output
-                notesByKeyCode[keyCode].key.sound.stop();
-
-                // Remove key highlight
-                notesByKeyCode[keyCode].key.html.className = 'key';
-            }
-        };
-
-        var setWaveform = function(event) {
-            for(var keyCode in notes) {
-                notes[keyCode].key.sound.osc.type = this.value;
-            }
-
-            // Unfocus selector so value is not accidentally updated again while playing keys
-            this.blur();
-        };
-
-        // Check for changes in the waveform selector and update all oscillators with the selected type
-        waveFormSelector.addEventListener('change', setWaveform);
-
-        window.addEventListener('keydown', playNote);
-        window.addEventListener('keyup', endNote);
-    }
-
-    window.addEventListener('load', function() {
-        createKeyboard(notesByKeyCode, 'keyboard');
-    });
-})();
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source1.noteOn(0);
+  source2.noteOn(0);
+}
+  */
