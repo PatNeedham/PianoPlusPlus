1c7c5bed04030be38e85d613d3c806f1f443120e
Pat Needham <needham.patrick@gmail.com>
2015-07-12 01:01:57 -0400

    Changed user model to incorporate Devise gem and dynamic nav bar buttons determined whether you are logged in

--- Gemfile
+++ Gemfile
@@ -45,3 +45,4 @@ end
 
 gem 'bootstrap-sass', '3.2.0.0'
 
+gem 'devise'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,9 @@ class UsersController < ApplicationController
 	def show
 	end
 
+	def index
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:first_name, :last_name, :username, :email, :password,
